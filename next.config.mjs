ead3ae18a80d12dd7d0a25aa3f76a9022aedf769
Mokshit <mokshit108@gmail.com>
2025-03-13 21:55:09 +0530

    Added setup files for render

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export',
+    // If you're using images, you need this configuration
+    images: {
+      unoptimized: true,
+    },
+  };
+  
 export default nextConfig;
+  
