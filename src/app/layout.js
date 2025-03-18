879441a44225db513ef1999e780bf2e2edd34e36
Mokshit <mokshit108@gmail.com>
2025-03-18 08:24:38 +0530

    Added Favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,6 +42,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
+ 
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} ${nunito.variable} ${roboto.variable} antialiased`}
       >
